36ec5dd53323f7aa025318636cfb69d962129494
Leedoseo <okok0840@naver.com>
2025-09-17 00:14:05 +0900

    onboarding_screen에 들어갈 page구현

---
+++ lib/onboarding/pages/welcome_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FeatureSlide(
+      icon: Icons.route_rounded,
+      title: "RouteLog에 오신 것을 환영합니다",
+      description: "러닝/산책 루트를 기록하고 \n거리-시간-페이스를 한눈에 확인하세요",
+    );
+  }
+}
