a7bf7811012b62a03550ce1ef3cdfd2fd9f5b3a2
Leedoseo <okok0840@naver.com>
2025-09-25 01:07:24 +0900

    finish_session_sheet.dart추가 map_placeholder.dart 수정 배럴에 추가

--- lib/features/record/widgets/map_placeholder.dart
+++ lib/features/record/widgets/map_placeholder.dart
@@ -1,24 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:routelog_project/core/widgets/soft_info_card.dart';
 
 class MapPlaceholder extends StatelessWidget {
-  const MapPlaceholder({super.key});
+  const MapPlaceholder({super.key, this.height = 180});
+
+  final double height;
 
   @override
   Widget build(BuildContext context) {
     final cs = Theme.of(context).colorScheme;
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 16),
-      decoration: BoxDecoration(
+
+    return SoftInfoCard(
+      padding: EdgeInsets.zero, // 카드 패딩 제거(지도를 꽉 채우는 룩)
+      child: ClipRRect(
         borderRadius: BorderRadius.circular(16),
-        border: Border.all(color: cs.outlineVariant),
-        gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [cs.primaryContainer, cs.secondaryContainer],
+        child: Container(
+          height: height,
+          decoration: BoxDecoration(
+            // 현재 앱 톤에 맞춘 은은한 그라데이션 + 테두리
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [cs.primaryContainer, cs.secondaryContainer],
+            ),
+            border: Border.all(color: cs.outlineVariant),
+          ),
+          child: Center(
+            child: Icon(
+              Icons.my_location_rounded,
+              size: 56,
+              color: cs.onPrimaryContainer.withOpacity(0.9),
+            ),
+          ),
         ),
-      ),
-      child: const Center(
-        child: Icon(Icons.my_location_rounded, size: 64),
       ),
     );
   }
